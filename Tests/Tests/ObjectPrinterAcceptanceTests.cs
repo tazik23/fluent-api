7f440b48e569f5afbc06ef979b17335df4a22ced
Vladimir Golubev <gor.prast@bk.ru>
2025-11-20 09:46:58 +0500

    test: add acceptance test

--- Tests/Tests/ObjectPrinterAcceptanceTests.cs
+++ Tests/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+﻿using System.Globalization;
 
 namespace ObjectPrinting.Tests
 {
@@ -10,18 +10,21 @@ namespace ObjectPrinting.Tests
         {
             var person = new Person { Name = "Alex", Age = 19 };
 
-            var printer = ObjectPrinter.For<Person>();
-                //1. Исключить из сериализации свойства определенного типа
-                //2. Указать альтернативный способ сериализации для определенного типа
-                //3. Для числовых типов указать культуру
-                //4. Настроить сериализацию конкретного свойства
-                //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
-                //6. Исключить из сериализации конкретного свойства
-            
-            string s1 = printer.PrintToString(person);
+            var printer = ObjectPrinter.For<Person>()
+                .Excluding<Guid>() // 1. Исключить из сериализации свойства определенного типа                      
+                .Printing<int>().Using(i => $"Int({i})") // 2. Указать альтернативный способ сериализации для определенного типа    
+                .Printing<double>().Using(CultureInfo.InvariantCulture) // 3. Для числовых типов указать культуру
+                .Printing(p => p.Name).Using(n => $"very cool name: {n}") // 4. Настроить сериализацию конкретного свойства
+                .Printing(p => p.Name).TrimToLength(10) // 5. Настроить обрезание строковых свойств
+                .Excluding(p => p.Age); // 6. Исключить конкретное свойство
 
-            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
-            //8. ...с конфигурированием
+            var s1 = printer.PrintToString(person);
+        
+            var s2 = person.PrintToString(); // 7. Синтаксический сахар — метод расширения
+        
+            var s3 = person.PrintToString(p => p
+                    .Printing<double>().Using(CultureInfo.GetCultureInfo("ru-RU")) // 8. ...с конфигурированием
+            );
         }
     }
 }
