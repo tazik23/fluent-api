cada7138d90f2dd1e1e305fafd84f262595efac6
Vladimir Golubev <gor.prast@bk.ru>
2025-11-20 19:21:37 +0500

    test: add culture tests

---
+++ Tests/Tests/ObjectPrintingTests.CultureTests.cs
@@ -0,0 +1,60 @@
+using System.Globalization;
+using FluentAssertions;
+using ObjectPrinting;
+using ObjectPrinting.PrintingConfigs.Extensions;
+
+namespace Tests.Tests;
+
+public partial class ObjectPrintingTests
+{
+    [TestFixture]
+    public class CultureTests
+    {
+        public class CultureTestClass
+        {
+            public decimal Decimal { get; set; }
+            public DateTime Date { get; set; }
+            public double Double { get; set; }
+        }
+        
+        private CultureTestClass testData;
+
+        [SetUp]
+        public void SetUp()
+        {
+            testData = new CultureTestClass
+            {
+                Decimal = 1234.56m,
+                Date = new DateTime(2023, 10, 15, 14, 30, 0),
+                Double = 1234.567
+            };
+        }
+        
+        [Test]
+        public void PrintToString_CustomCulture_ShouldApplyFormatting()
+        {
+            var printer = ObjectPrinter.For<CultureTestClass>()
+                .Printing<decimal>().Using(new CultureInfo("de-DE"));
+
+            var result = printer.PrintToString(testData);
+
+            result.Should().Contain("Decimal = 1234,56");
+        }
+        
+        [Test]
+        public void PrintToString_MultipleCultures_ShouldApplyAll()
+        {
+            var printer = ObjectPrinter.For<CultureTestClass>()
+                .Printing<double>().Using(new CultureInfo("es-ES"))
+                .Printing<decimal>().Using(new CultureInfo("en-US"))
+                .Printing<DateTime>().Using(new CultureInfo("fr-FR"));
+
+            var result = printer.PrintToString(testData);
+
+            result.Should()
+                .Contain("Decimal = 1234.56")
+                .And.Contain("Date = 15/10/2023 14:30:00")
+                .And.Contain("Double = 1234,567");
+        }
+    }
+}
